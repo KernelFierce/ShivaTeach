d5e0ba4553a0c610fd5c7fd1ed9a8393e26493c7
Ankur Kakkar <kakkar.ankur.4@gmail.com>
2025-09-24 21:29:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7755222125-58bbd",
+  "appId": "1:252966941232:web:c642c1264c496b8ec0f3f5",
+  "apiKey": "AIzaSyBPXf-ouuNjHOrPe7W31f4ZfLu3u8jPpIw",
+  "authDomain": "studio-7755222125-58bbd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "252966941232"
+};
